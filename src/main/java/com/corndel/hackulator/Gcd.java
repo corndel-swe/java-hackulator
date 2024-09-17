83b636382c05d155d6a72322c201a29735f851aa
Simon Haines <nimon@hotmail.co.uk>
2024-09-17 16:26:46 +0100

    Update Gcd.java

--- src/main/java/com/corndel/hackulator/Gcd.java
+++ src/main/java/com/corndel/hackulator/Gcd.java
@@ -6,7 +6,9 @@ public class Gcd {
    * Works out the greatest common divisor of two numbers Uses the euclidean algorithm:
    * https://www.youtube.com/watch?v=JUzYl1TYMcU
    *
-   * <p>gcd(8, 12) => 4
+   * <p>
+   * gcd(8, 12) => 4
+   * gcd(15, 10) => 5
    */
   static int gcd(int a, int b) {
     while (a != 0) {
